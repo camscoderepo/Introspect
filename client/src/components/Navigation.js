1bf7913462d3e653e38388427e0650d86c076f29
Cameron Roman <camroman95@gmail.com>
2019-10-15 02:51:16 -0500

    changed class to className

--- client/src/components/Navigation.js
+++ client/src/components/Navigation.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 const Navigation = () => {
     return(
-<header class="bg-orange-90 fixed w-100 ph3 pv3 pv4-ns ph4-m ph5-l">
-  <nav class="f6 fw6 ttu tracked">
-    <li class="tc db tt" href="#" title="Home">Introspect</li>
+<header className="bg-orange-90 fixed w-100 ph3 pv3 pv4-ns ph4-m ph5-l">
+  <nav className="f6 fw6 ttu tracked">
+    <li className="tc db tt" href="#" title="Home">Introspect</li>
   </nav>
 </header>
     ); 
