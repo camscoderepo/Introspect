fea21f9702afc8fbdaef1e8e79de3f11fcd97c34
Cameron Roman <camroman95@gmail.com>
2019-10-15 02:47:10 -0500

    started working on navigation

--- client/src/components/Navigation.js
+++ client/src/components/Navigation.js
@@ -2,15 +2,11 @@ import React from 'react';
 
 const Navigation = () => {
     return(
-    <nav className="dt dt--fixed w-100 border-box pa3 ph5-ns bb b--black-10">
-        <a className="dtc v-mid link dim w-25" href="#" title="Home">
-        </a>
-        <div id="menu" class="static-ns absolute mt5 mt0-ns left-100 db dtc-ns v-mid w-50 w-100-ns w-75-ns h-80 tr">
-            <a class="link dim dark-gray tr tl-ns f3 f5-ns db dib-ns mr3 mr4-ns" href="#" title="Home">Home</a>
-            <a class="link dim dark-gray tr tl-ns f3 f5-ns db dib-ns mr3 mr4-ns" href="#" title="About">About</a>
-            <a class="link dim dark-gray tr tl-ns f3 f5-ns db dib-ns mr3 mr0-ns" href="#" title="Contact">Contact</a>
-        </div>
-    </nav>
+<header class="bg-orange-90 fixed w-100 ph3 pv3 pv4-ns ph4-m ph5-l">
+  <nav class="f6 fw6 ttu tracked">
+    <li class="tc db tt" href="#" title="Home">Introspect</li>
+  </nav>
+</header>
     ); 
 }
 
